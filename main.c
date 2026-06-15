e5e8def6bb5a54cb5fc03bc18830ad3a412fd5dc
Janeiloson Paulino de Araujo <146784212+janeilson-araujo@users.noreply.github.com>
2026-06-15 22:47:42 +0000

    adição do struct tarefa e inicio do formato visual  do menu

--- main.c
+++ main.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+struct task
+{
+    char descricao[256];
+    int prioridade;
+    bool situacao;
+};
+
+void menu()
+{
+    printf("\nEscolha uma opção:\n");
+    printf("1. Adicionar tarefa\n");
+    printf("2. Listar tarefas\n");
+    printf("3. Listar tarefas pendentes\n");
+    printf("4. Listar tarefas concluídas\n");
+    printf("0. Sair\n");
+}
+
+void adicionarTarefa()
+{
+    
+}
 
 int main()
 {
@@ -9,9 +32,39 @@ int main()
 
     int opcao;
 
-    do {
-        scanf("%d", &opcao);
-    } while (opcao != 0);
+    printf("\nEscolha uma opção:\n");
+    printf("1. Adicionar tarefa\n");
+    printf("2. Listar tarefas\n");
+    printf("3. Listar tarefas pendentes\n");
+    printf("4. Listar tarefas concluídas\n");
+    printf("0. Sair\n");
 
+    
+    while (true){
+        scanf("%d", &opcao);
+        switch (opcao){
+        case 1:
+            printf("Adicionar tarefa selecionada.\n");
+                adicionarTarefa();
+    
+        case 2:
+            printf("Listar tarefas selecionada.\n");
+            // Lógica para listar todas as tarefas
+    
+        case 3:
+            printf("Listar tarefas pendentes selecionada.\n");
+            // Lógica para listar tarefas pendentes
+            
+        case 4:
+            printf("Listar tarefas concluídas selecionada.\n");
+            // Lógica para listar tarefas concluídas
+            
+        case 0:
+            printf("Saindo do programa. Até logo!\n");
+            exit(0);
+        default:
+            printf("Opção inválida. Por favor, tente novamente.\n");
+        }
+    }
     return 0;
 }
