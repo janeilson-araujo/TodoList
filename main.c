fe9c855ca055125b997344b951eaeb99c86a88d1
Janeiloson Paulino de Araujo <146784212+janeilson-araujo@users.noreply.github.com>
2026-06-16 02:37:50 +0000

    tratamento das entradas da função adicionarTarefa, criar e salvar um arquivo.txt com as taefas na função adicionar tarefa, break para parar o switch case no main. obs: falta tratar a entrada de opções no main

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct task
 {
@@ -8,22 +10,55 @@ struct task
     bool situacao;
 };
 
-void menu()
-{
-    printf("\nEscolha uma opção:\n");
-    printf("1. Adicionar tarefa\n");
-    printf("2. Listar tarefas\n");
-    printf("3. Listar tarefas pendentes\n");
-    printf("4. Listar tarefas concluídas\n");
-    printf("0. Sair\n");
-}
-
 void adicionarTarefa()
 {
-    
+
+    FILE *arq = fopen("tarefas.txt", "r");
+    if (arq == NULL)
+    {
+        FILE *arq = fopen("tarefas.txt", "w");
+    };
+    fclose(arq);
+
+    struct task novaTarefa;
+
+    do
+    {
+        printf("Digite a descrição da tarefa: ");
+        fgets(novaTarefa.descricao, sizeof(novaTarefa.descricao), stdin);
+        novaTarefa.descricao[strcspn(novaTarefa.descricao, "\n")] = '\0';
+        // tratamento de erros para descrição da tarefa
+        if (strlen(novaTarefa.descricao) == 0)
+        {
+            printf("Descrição inválida. Por favor, digite uma descrição para a tarefa.\n");
+        }
+        if (strlen(novaTarefa.descricao) > 255)
+        {
+            printf("Descrição muito longa. Por favor, digite uma descrição com no máximo 255 caracteres.\n");
+        }
+
+    } while (strlen(novaTarefa.descricao) == 0 || strlen(novaTarefa.descricao) > 255);
+
+    do
+    {
+        printf("Digite a prioridade da tarefa (1-5): ");
+        scanf("%d", &novaTarefa.prioridade);
+        if (novaTarefa.prioridade < 1 || novaTarefa.prioridade > 5)
+        {
+            printf("Prioridade inválida. Por favor, digite um valor entre 1 e 5.\n");
+        }
+        getchar();
+
+    } while (novaTarefa.prioridade < 1 || novaTarefa.prioridade > 5);
+
+    novaTarefa.situacao = false;
+
+    FILE *arquivo = fopen("tarefas.txt", "a");
+    fprintf(arquivo, "%s, %d, %d\n", novaTarefa.descricao, novaTarefa.prioridade, novaTarefa.situacao);
+    fclose(arquivo);
 }
 
-int main()
+void main()
 {
     printf("+--------------------------------------+\n");
     printf("|              TODO LIST               |\n");
@@ -32,39 +67,46 @@ int main()
 
     int opcao;
 
-    printf("\nEscolha uma opção:\n");
-    printf("1. Adicionar tarefa\n");
-    printf("2. Listar tarefas\n");
-    printf("3. Listar tarefas pendentes\n");
-    printf("4. Listar tarefas concluídas\n");
-    printf("0. Sair\n");
+    while (true)
+    {
 
-    
-    while (true){
-        scanf("%d", &opcao);
-        switch (opcao){
+        printf("\nEscolha uma opção:\n");
+        printf("1. Adicionar tarefa\n");
+        printf("2. Listar tarefas\n");
+        printf("3. Listar tarefas pendentes\n");
+        printf("4. Listar tarefas concluídas\n");
+        printf("0. Sair\n");
+
+        scanf("digite sua opção: %d", &opcao);
+
+        getchar();
+
+        switch (opcao)
+        {
         case 1:
             printf("Adicionar tarefa selecionada.\n");
-                adicionarTarefa();
-    
+            adicionarTarefa();
+            break;
+
         case 2:
             printf("Listar tarefas selecionada.\n");
-            // Lógica para listar todas as tarefas
-    
+            break;
+
         case 3:
             printf("Listar tarefas pendentes selecionada.\n");
-            // Lógica para listar tarefas pendentes
-            
+            break;
+
         case 4:
             printf("Listar tarefas concluídas selecionada.\n");
-            // Lógica para listar tarefas concluídas
-            
+            break;
+
         case 0:
             printf("Saindo do programa. Até logo!\n");
             exit(0);
+
         default:
             printf("Opção inválida. Por favor, tente novamente.\n");
+            break;
         }
     }
-    return 0;
 }
