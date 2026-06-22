7994d46e553feb507abe63eb6a00d13e474c52a4
Janeiloson Paulino de Araujo <146784212+janeilson-araujo@users.noreply.github.com>
2026-06-22 02:22:59 +0000

    implementação da função marcarTarefaConcluida adiconada

--- main.c
+++ main.c
@@ -57,6 +57,84 @@ void adicionarTarefa()
     printf("Tarefa adicionada com sucesso!\n");
 }
 
+void marcarTarefaConcluida()
+{
+    FILE *abrirArquivo = fopen("tarefas.txt", "r");
+    if (abrirArquivo == NULL)
+    {
+        printf("Erro ao abrir o arquivo de tarefas.\n");
+        return;
+    }
+    FILE *arquivoTemp = fopen("tarefas_temp.txt", "w");
+
+    int id = 1;
+    char situacao_temp[2];
+
+    struct task tarefaEditar;
+
+    printf("\ntarefas:\n");
+    while (fscanf(abrirArquivo, "%255[^,], %d, %d\n", tarefaEditar.descricao, &tarefaEditar.prioridade, &tarefaEditar.situacao) != EOF)
+    {
+        printf("ID: %d | Descrição: %s | Prioridade: %d | Situação: %s\n", id, tarefaEditar.descricao, tarefaEditar.prioridade, tarefaEditar.situacao ? "Concluída" : "Pendente");
+        id++;
+    }
+
+    rewind(abrirArquivo);
+    int idEdicao;
+
+    do
+    {
+        printf("Digite o ID da tarefa que deseja editar: ");
+        scanf("%d", &idEdicao);
+        getchar();
+        if (idEdicao < 1 || idEdicao >= id)
+        {
+            printf("ID inválido. Por favor, digite um ID válido.\n");
+        }
+    } while (idEdicao < 1 || idEdicao >= id);
+
+    id = 1;
+
+    while (fscanf(abrirArquivo, "%255[^,], %d, %d\n", tarefaEditar.descricao, &tarefaEditar.prioridade, &tarefaEditar.situacao) != EOF)
+    {
+        if (id == idEdicao)
+        {
+            do
+            {
+                printf("Deseja marcar a tarefa como concluída? (y - SIM, n - NÂO): ");
+                fgets(situacao_temp, sizeof(situacao_temp), stdin);
+                situacao_temp[strcspn(situacao_temp, "\n")] = '\0';
+                if (strcmp(situacao_temp, "y") == 0 || strcmp(situacao_temp, "Y") == 0)
+                {
+                    tarefaEditar.situacao = 1;
+                }
+                else if (strcmp(situacao_temp, "n") == 0 || strcmp(situacao_temp, "N") == 0)
+                {
+                    tarefaEditar.situacao = 0;
+                }
+                else
+                {
+                    printf("Opção inválida. Por favor, digite 'y' para SIM ou 'n' para NÃO.\n");
+                }
+
+            } while (situacao_temp[0] != 'y' && situacao_temp[0] != 'Y' && situacao_temp[0] != 'n' && situacao_temp[0] != 'N');
+
+            fprintf(arquivoTemp, "%s, %d, %d\n", tarefaEditar.descricao, tarefaEditar.prioridade, tarefaEditar.situacao);
+        }
+
+        else
+        {
+            fprintf(arquivoTemp, "%s, %d, %d\n", tarefaEditar.descricao, tarefaEditar.prioridade, tarefaEditar.situacao);
+        }
+        id++;
+    }
+
+    fclose(abrirArquivo);
+    fclose(arquivoTemp);
+    remove("tarefas.txt");
+    rename("tarefas_temp.txt", "tarefas.txt");
+}
+
 void editarTarefa()
 {
     FILE *abrirArquivo = fopen("tarefas.txt", "r");
@@ -293,6 +371,7 @@ int main()
         case 2:
             system("clear");
             printf("\nMarcar tarefas como concluidas selecionado.\n");
+            marcarTarefaConcluida();
             break;
 
         case 3:
